1bec2fc2799d36c0352db2cc59bfcbaf1a69d8cd
Cássio Santos <maluqueiro@gmail.com>
2026-07-24 17:44:27 -0300

    feat(tokens): adiciona cores SwiftUI

---
+++ tokens/swift/CIATAColors.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+public enum CIATAColors {
+    public static let amarelo = Color(red: 1.0, green: 208.0 / 255.0, blue: 0.0)
+    public static let azul = Color(red: 0.0, green: 92.0 / 255.0, blue: 153.0 / 255.0)
+    public static let azulEscuro = Color(red: 22.0 / 255.0, green: 54.0 / 255.0, blue: 94.0 / 255.0)
+    public static let cinza = Color(red: 179.0 / 255.0, green: 179.0 / 255.0, blue: 179.0 / 255.0)
+}
