c13d0883fcc0ab6761caccf5274f03cd3d57a461
Cássio Santos <maluqueiro@gmail.com>
2026-09-02 17:12:48 -0300

    feat: adiciona componente Checkbox para Laravel

---
+++ components/checkbox/laravel/checkbox.blade.php
@@ -0,0 +1,50 @@
+@props([
+    'id',
+    'name',
+    'label',
+    'value' => '1',
+    'checked' => false,
+    'help' => null,
+    'error' => null,
+    'required' => false,
+    'disabled' => false,
+])
+
+@php
+    $helpId = $help ? "{$id}-help" : null;
+    $errorId = $error ? "{$id}-error" : null;
+    $describedBy = collect([$helpId, $errorId])->filter()->implode(' ');
+    $isChecked = old($name, $checked);
+@endphp
+
+<div class="ciata-checkbox">
+    <div class="ciata-checkbox__control-row">
+        <input
+            id="{{ $id }}"
+            name="{{ $name }}"
+            type="checkbox"
+            value="{{ $value }}"
+            class="ciata-checkbox__control"
+            @checked($isChecked)
+            @if($required) required @endif
+            @if($disabled) disabled @endif
+            @if($describedBy) aria-describedby="{{ $describedBy }}" @endif
+            @if($error) aria-invalid="true" aria-errormessage="{{ $errorId }}" @endif
+            {{ $attributes->except(['class']) }}
+        >
+        <label class="ciata-checkbox__label" for="{{ $id }}">
+            {{ $label }}
+            @if($required)
+                <span class="ciata-checkbox__required">(obrigatório)</span>
+            @endif
+        </label>
+    </div>
+
+    @if($help)
+        <div id="{{ $helpId }}" class="ciata-checkbox__help">{{ $help }}</div>
+    @endif
+
+    @if($error)
+        <div id="{{ $errorId }}" class="ciata-checkbox__error">{{ $error }}</div>
+    @endif
+</div>
