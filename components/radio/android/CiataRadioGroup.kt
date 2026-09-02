fa9fbf6bce988f96dd6f138f37c0fdb4a155314b
Cássio Santos <maluqueiro@gmail.com>
2026-09-02 17:23:19 -0300

    fix: remove import desnecessário do Radio Compose

--- components/radio/android/CiataRadioGroup.kt
+++ components/radio/android/CiataRadioGroup.kt
@@ -1,6 +1,5 @@
 package org.ciata.ds.radio
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
