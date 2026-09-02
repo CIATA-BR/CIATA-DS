165d1d20eb56c81e2c0fd2c8c4dbaedb2de8ce68
Cássio Santos <maluqueiro@gmail.com>
2026-09-02 16:27:47 -0300

    feat: adiciona componente Blade TextField

---
+++ components/text-field/laravel/text-field.blade.php
@@ -0,0 +1,81 @@
+@props([
+    'id',
+    'name',
+    'label',
+    'type' => 'text',
+    'value' => null,
+    'help' => null,
+    'error' => null,
+    'required' => false,
+    'readonly' => false,
+    'disabled' => false,
+    'optionalLabel' => null,
+    'autocomplete' => null,
+    'inputmode' => null,
+    'placeholder' => null,
+    'maxlength' => null,
+    'multiline' => false,
+    'rows' => 4,
+])
+
+@php
+    $helpId = $help ? "{$id}-help" : null;
+    $errorId = $error ? "{$id}-error" : null;
+    $describedBy = collect([$helpId, $errorId])->filter()->implode(' ');
+@endphp
+
+<div class="ciata-field">
+    <label class="ciata-field__label" for="{{ $id }}">
+        {{ $label }}
+        @if($required)
+            <span class="ciata-field__required">(obrigatório)</span>
+        @elseif($optionalLabel)
+            <span class="ciata-field__optional">({{ $optionalLabel }})</span>
+        @endif
+    </label>
+
+    @if($multiline)
+        <textarea
+            id="{{ $id }}"
+            name="{{ $name }}"
+            rows="{{ $rows }}"
+            class="ciata-field__control"
+            @if($required) required @endif
+            @if($readonly) readonly @endif
+            @if($disabled) disabled @endif
+            @if($maxlength) maxlength="{{ $maxlength }}" @endif
+            @if($autocomplete) autocomplete="{{ $autocomplete }}" @endif
+            @if($inputmode) inputmode="{{ $inputmode }}" @endif
+            @if($placeholder) placeholder="{{ $placeholder }}" @endif
+            @if($describedBy) aria-describedby="{{ $describedBy }}" @endif
+            @if($error) aria-invalid="true" aria-errormessage="{{ $errorId }}" @endif
+            {{ $attributes->except(['class']) }}
+        >{{ old($name, $value) }}</textarea>
+    @else
+        <input
+            id="{{ $id }}"
+            name="{{ $name }}"
+            type="{{ $type }}"
+            value="{{ old($name, $value) }}"
+            class="ciata-field__control"
+            @if($required) required @endif
+            @if($readonly) readonly @endif
+            @if($disabled) disabled @endif
+            @if($maxlength) maxlength="{{ $maxlength }}" @endif
+            @if($autocomplete) autocomplete="{{ $autocomplete }}" @endif
+            @if($inputmode) inputmode="{{ $inputmode }}" @endif
+            @if($placeholder) placeholder="{{ $placeholder }}" @endif
+            @if($describedBy) aria-describedby="{{ $describedBy }}" @endif
+            @if($error) aria-invalid="true" aria-errormessage="{{ $errorId }}" @endif
+            {{ $attributes->except(['class']) }}
+        >
+    @endif
+
+    @if($help)
+        <div id="{{ $helpId }}" class="ciata-field__help">{{ $help }}</div>
+    @endif
+
+    @if($error)
+        <div id="{{ $errorId }}" class="ciata-field__error">{{ $error }}</div>
+    @endif
+</div>
