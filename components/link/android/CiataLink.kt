2e5086e57ffaa469c8e8bf0b7286db486a941bc0
Cássio Santos <maluqueiro@gmail.com>
2026-09-02 19:03:43 -0300

    fix: remove import não utilizado do Link Android

--- components/link/android/CiataLink.kt
+++ components/link/android/CiataLink.kt
@@ -1,6 +1,5 @@
 package org.ciata.ds.link
 
-import androidx.compose.foundation.text.InlineTextContent
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
