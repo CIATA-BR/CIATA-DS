c66311de57c6ad2abdf63eba8bd841c831d65cda
Cássio Santos <maluqueiro@gmail.com>
2026-09-03 12:27:34 -0300

    fix: sincroniza seleção controlada do Tabs no Flutter

--- components/tabs/flutter/ciata_tabs.dart
+++ components/tabs/flutter/ciata_tabs.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 /// Implementação experimental do CMP-0013 Tabs para Flutter.
-class CiataTabs extends StatelessWidget {
+class CiataTabs extends StatefulWidget {
   const CiataTabs({
     super.key,
     required this.labels,
@@ -18,20 +18,56 @@ class CiataTabs extends StatelessWidget {
   final Widget Function(int) panelBuilder;
 
   @override
+  State<CiataTabs> createState() => _CiataTabsState();
+}
+
+class _CiataTabsState extends State<CiataTabs>
+    with SingleTickerProviderStateMixin {
+  late TabController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(
+      length: widget.labels.length,
+      initialIndex: widget.selectedIndex,
+      vsync: this,
+    );
+  }
+
+  @override
+  void didUpdateWidget(covariant CiataTabs oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.labels.length != widget.labels.length) {
+      _controller.dispose();
+      _controller = TabController(
+        length: widget.labels.length,
+        initialIndex: widget.selectedIndex,
+        vsync: this,
+      );
+    } else if (_controller.index != widget.selectedIndex) {
+      _controller.index = widget.selectedIndex;
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: labels.length,
-      initialIndex: selectedIndex,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          TabBar(
-            onTap: onSelectionChange,
-            tabs: [for (final label in labels) Tab(text: label)],
-          ),
-          panelBuilder(selectedIndex),
-        ],
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        TabBar(
+          controller: _controller,
+          onTap: widget.onSelectionChange,
+          tabs: [for (final label in widget.labels) Tab(text: label)],
+        ),
+        widget.panelBuilder(widget.selectedIndex),
+      ],
     );
   }
 }
