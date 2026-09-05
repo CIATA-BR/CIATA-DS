075814c206ae893bd06b93798bc72021fa78f210
Cássio Santos <maluqueiro@gmail.com>
2026-09-05 12:58:18 -0300

    fix: torna heading do Bottom Sheet configurável

--- components/bottom-sheet/laravel/bottom-sheet.blade.php
+++ components/bottom-sheet/laravel/bottom-sheet.blade.php
@@ -1,8 +1,11 @@
 @props([
     'id',
     'title',
+    'titleLevel' => 2,
     'triggerLabel' => 'Abrir',
 ])
+
+@php($titleLevel = max(1, min(6, (int) $titleLevel)))
 
 <div class="ciata-bottom-sheet" data-ciata-bottom-sheet>
     <button type="button" class="ciata-bottom-sheet__trigger" aria-haspopup="dialog" aria-controls="{{ $id }}">
@@ -12,7 +15,7 @@
     <dialog id="{{ $id }}" class="ciata-bottom-sheet__dialog" aria-labelledby="{{ $id }}-title">
         <section class="ciata-bottom-sheet__surface">
             <header class="ciata-bottom-sheet__header">
-                <h2 id="{{ $id }}-title" tabindex="-1">{{ $title }}</h2>
+                <h{{ $titleLevel }} id="{{ $id }}-title" tabindex="-1">{{ $title }}</h{{ $titleLevel }}>
                 <button type="button" class="ciata-bottom-sheet__close">Fechar</button>
             </header>
             <div class="ciata-bottom-sheet__content">{{ $slot }}</div>
