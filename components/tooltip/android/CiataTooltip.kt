73177635cae4e24ecf47149508b23e3eadb5b359
Cássio Santos <maluqueiro@gmail.com>
2026-09-05 12:54:12 -0300

    fix: explicita contrato experimental do Tooltip Android

--- components/tooltip/android/CiataTooltip.kt
+++ components/tooltip/android/CiataTooltip.kt
@@ -1,12 +1,15 @@
 package org.ciata.ds.tooltip
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.Text
 import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
 import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
 
 /** Implementação experimental do CMP-0017 Tooltip para Jetpack Compose. */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CiataTooltip(
     text: String,
@@ -15,7 +18,7 @@ fun CiataTooltip(
     require(text.isNotBlank()) { "text não pode ser vazio." }
 
     TooltipBox(
-        positionProvider = androidx.compose.material3.TooltipDefaults.rememberPlainTooltipPositionProvider(),
+        positionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(),
         tooltip = { PlainTooltip { Text(text) } },
         state = rememberTooltipState(),
     ) {
