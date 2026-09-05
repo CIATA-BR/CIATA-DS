82ce086b846bdfb0b9e58f04710552994475c6d6
Cássio Santos <maluqueiro@gmail.com>
2026-09-05 18:12:29 -0300

    fix: valida propriedades do Dialog Laravel

--- components/modal-dialog/laravel/dialog.blade.php
+++ components/modal-dialog/laravel/dialog.blade.php
@@ -4,16 +4,26 @@
     'description' => null,
 ])
 
+@php
+    $id = trim((string) $id);
+    $title = trim((string) $title);
+    $description = $description === null ? null : trim((string) $description);
+
+    if ($id === '' || $title === '') {
+        throw new InvalidArgumentException('id e title não podem ser vazios.');
+    }
+@endphp
+
 <dialog
     id="{{ $id }}"
     class="ciata-dialog"
     aria-labelledby="{{ $id }}-title"
-    @if($description) aria-describedby="{{ $id }}-description" @endif
+    @if($description !== null && $description !== '') aria-describedby="{{ $id }}-description" @endif
     {{ $attributes }}
 >
     <h2 id="{{ $id }}-title" class="ciata-dialog__title">{{ $title }}</h2>
 
-    @if($description)
+    @if($description !== null && $description !== '')
         <p id="{{ $id }}-description">{{ $description }}</p>
     @endif
 
